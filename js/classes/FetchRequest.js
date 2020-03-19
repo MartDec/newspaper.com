ea73db72bdd3499c845c809132d81c87f6c66a7f
Martin Déclert <declertmartin@gmail.com>
2020-03-19 16:16:09 +0100

    Suppression des console.log

--- js/classes/FetchRequest.js
+++ js/classes/FetchRequest.js
@@ -22,10 +22,7 @@ export class FetchRequest {
             params.body = JSON.stringify(this.data);
 
         return fetch(this.url, params)
-            .then(res => {
-                console.log(res);
-                return res.ok ? res.json() : {error: `${res.status}: ${res.statusText}`}
-            })
+            .then(res => res.ok ? res.json() : {error: `${res.status}: ${res.statusText}`})
             .then(data => {
                 if (typeof data.error === 'undefined')
                     return data;
